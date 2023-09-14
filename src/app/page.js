5ff08ea294a745fa0e105a304bd343eaca3d7c08
Sanchiz <chrisrosy29@mail.ru>
2023-09-14 21:47:45 +0300

    responsivnes + bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,16 @@ export default function Home() {
     <main className="h-screen w-screen flex flex-col items-center p-4 gap-y-10 overflow-x-hidden">
       <div className="w-[60%] p-4 m-10 flex flex-col items-center justify-between">
         <img src="/favicon-32x32.png" className="m-6 h-24 w-24" alt="Flowbite Logo" />
-        <div className="text-[60px]  text-center ">Федерация скейтбординга контесты</div>
+        <div className="lg:text-[60px] text-[40px]  text-center ">
+          Федерация скейтбординга контесты
+        </div>
       </div>
 
-      <div className="flex items-center justify-evenly flex-col w-[80%] h-[60%] gap-y-14">
+      <div className="flex items-center justify-evenly flex-col w-[80%] h-[60%] my-10 gap-y-14">
         <div className="flex-1 text-center flex w-full text-3xl md:text-5xl lg:text-5xl items-center justify-center ">
           <Link href="/men-park-results">Парни парк</Link>
         </div>
+
         <Separator className=" bg-gray-300 w-[50%]" />
 
         <div className="flex-1 text-center flex w-full text-3xl md:text-5xl lg:text-5xl items-center justify-center ">
@@ -21,25 +24,23 @@ export default function Home() {
         </div>
 
         <Separator className=" bg-gray-300 w-[50%]" />
-        <div
-          className="flex-1 text-center flex w-full text-3xl md:text-5xl lg:text-5xl items-center justify-center <Separator />
-">
+
+        <div className="flex-1 text-center flex w-full text-3xl md:text-5xl lg:text-5xl items-center justify-center">
           <Link href="/men-street-results">Парни улица</Link>
         </div>
 
         <Separator className=" bg-gray-300 w-[50%]" />
-        <div
-          className="flex-1 text-center flex w-full text-3xl md:text-5xl lg:text-5xl items-center justify-center <Separator />
-">
+
+        <div className="flex-1 text-center flex w-full text-3xl md:text-5xl lg:text-5xl items-center justify-center">
           <Link href="/women-street-results">Девушки улица</Link>
         </div>
       </div>
 
-      <footer className="p-4 w-[80%] bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
+      <footer className="p-4 w-[80%] bg-white rounded-lg shadow md:px-6 md:py-8 my-10 dark:bg-gray-800">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a href="#" target="_blank" className="flex items-center mb-4 sm:mb-0">
             <img src="/favicon-32x32.png" className="mr-4 h-8" alt="Flowbite Logo" />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-base lg:text-xl font-semibold whitespace-nowrap dark:text-white">
               Федерация Скейтбординга России
             </span>
           </a>
